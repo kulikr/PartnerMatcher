e6aa0f6dbd8347d5c0ded7af762c1e1c0066ca36
kulikr <kulikr@post.bgu.ac.il>
2017-01-08 12:20:06 +0200

    first full commit

---
+++ PartnerMatcher/Search_window.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PartnerMatcher
+{
+    /// <summary>
+    /// Interaction logic for Search_window.xaml
+    /// </summary>
+    public partial class Search_window : Window
+    {
+        OleDbConnection connection;
+        Dictionary<string, string> interestArea;
+        public Search_window()
+        {
+            InitializeComponent();
+
+
+
+            string connectionString = PartnerMatcher.Properties.Settings.Default.DBconnection;
+            connection = new OleDbConnection(connectionString);
+            try
+            {
+                interestArea = new Dictionary<string, string>();
+                connection.Open();
+                getPossibleInterestAreas();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        /// <summary>
+        /// This function implements the query which gets the current interest areas in the DB
+        /// </summary>
+        /// <param name="connection">The DB connection</param>
+        private void getPossibleInterestAreas()
+        {
+            OleDbCommand command = new OleDbCommand("select * from InterestArea", connection);
+            OleDbDataAdapter tableAdapter = new OleDbDataAdapter(command);
+            DataTable dt = new DataTable();
+            tableAdapter.Fill(dt);
+            DataRow[] rows = dt.Select();
+            HashSet<string> areas = new HashSet<string>();
+            foreach (DataRow dr in rows)
+            {
+                areas.Add(dr[1].ToString());
+                interestArea.Add(dr[1].ToString(), dr[0].ToString());
+            }
+            box_interest.ItemsSource = areas;
+        }
+
+        /// <summary>
+        /// This function sets the possible location of partnership for each interest area
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void box_interestSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+
+            OleDbCommand command = new OleDbCommand("select city from Partnerships" + interestArea[box_interest.SelectedItem.ToString()], connection);
+            OleDbDataAdapter tableAdapter = new OleDbDataAdapter(command);
+            DataTable dt = new DataTable();
+            tableAdapter.Fill(dt);
+            DataRow[] rows = dt.Select();
+            if (rows.Length == 0)
+            {
+                box_location.Visibility = Visibility.Hidden;
+                header_location.Visibility = Visibility.Hidden;
+                MessageBox.Show(" לא קיימות מודעות שותפות בתחום זה. אנא נסה במועד מאוחר יותר");
+            }
+            else
+            {
+                box_location.Visibility = Visibility.Visible;
+                header_location.Visibility = Visibility.Visible;
+            }
+            HashSet<string> locations = new HashSet<string>();
+            foreach (DataRow dr in rows)
+            {
+                locations.Add(dr[0].ToString());
+            }
+            box_location.ItemsSource = locations;
+        }
+
+        private void box_location_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (box_location.SelectedItem != null)
+            {
+                if (box_location.SelectedItem.ToString() != "")
+                {
+                    b_showResults.Visibility = Visibility.Visible;
+                }
+                else
+                    b_showResults.Visibility = Visibility.Hidden;
+            }
+            else
+                b_showResults.Visibility = Visibility.Hidden;
+        }
+
+        private void b_showResults_Click(object sender, RoutedEventArgs e)
+        {
+            OleDbCommand command = new OleDbCommand("select * from Partnerships" + interestArea[box_interest.SelectedItem.ToString()] + " where City = '" + box_location.SelectedItem + "'", connection);
+            OleDbDataAdapter tableAdapter = new OleDbDataAdapter(command);
+            DataTable dt = new DataTable();
+            tableAdapter.Fill(dt);
+            DataRow[] rows = dt.Select();
+            dataGrid.CanUserAddRows = false;
+            dataGrid.AutoGenerateColumns = true;
+            this.dataGrid.ItemsSource = dt.AsDataView();
+
+        }
+    }
+}
